6c52d058f21d0bb7fc5be8dde880d0ed40de9a2f
timvm1108 <timvanmaren@hotmail.com>
2021-02-22 18:02:47 -0700

    Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for mysite project.
 
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
